cbbeb5590bc111c323a85d4a9684f22849f5e13e
danakal <dana.kalcikova@centrum.cz>
2020-09-02 19:50:34 +0200

    Second button and totally random RGB color

--- project_change_color_background-master/js/script.js
+++ project_change_color_background-master/js/script.js
@@ -1,9 +1,18 @@
 'use strict';
 
-document.querySelector('.btn').addEventListener('click',changeColor);
+document.querySelector('#btn-given').addEventListener('click',changeColor);
+document.querySelector('#btn-random').addEventListener('click',randomColor);
+
 
 function changeColor() {
     const colors=['red', 'green', 'blue', 'yellow', 'pink', 'orange'];
     let i= Math.floor(Math.random()*colors.length);
     document.querySelector('body').style.backgroundColor = colors[i];
 }
+
+function randomColor() {
+    let red = (Math.random() * 255).toFixed(0);
+    let green = (Math.random() * 255).toFixed(0);
+    let blue = (Math.random() * 255).toFixed(0);
+    document.querySelector('body').style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+    }
